78bddd9715f04824945425cc970f6f60d098a2f4
saad.ahmed1991 <ahmed.caparzo@gmail.com>
2023-10-21 11:16:39 +0100

    first commit

---
+++ components/PageHeader.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import logo from "../assets/logo.jpg";
+import { BellIcon, MenuIcon, Mic, Search, Upload, User } from "lucide-react";
+import Button from "./Button";
+
+const PageHeader = () => {
+  const [fullWidthSearch, setFullWidthSearch] = useState(false);
+  return (
+    <div
+      className={`gap-10 lg:gap-20 justify-between items-center pt-2 mb-6 mx-4 ${
+        fullWidthSearch ? "hidden" : "flex"
+      }`}
+    >
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant={"ghost"} size={"icon"}>
+          <MenuIcon />
+        </Button>
+        <Link href="#">
+          <Image className="w-40 h-16" src={logo} alt="" />
+        </Link>
+      </div>
+      <form
+        className={`md:flex gap-4 flex-grow justify-center ${
+          fullWidthSearch ? "flex" : "hidden"
+        }`}
+      >
+        <div className="flex flex-grow max-w-[600px] ">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border-secondary-border border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" className="flex-shrink-0" size={"icon"}>
+          <Mic />
+        </Button>
+      </form>
+      <div
+        className={`flex-shrink-0 md:gap-2 ${
+          fullWidthSearch ? "hidden" : "flex"
+        } `}
+      >
+        <Button
+          onClick={() => setFullWidthSearch(true)}
+          className="md:hidden"
+          size={"icon"}
+          variant={"ghost"}
+        >
+          <Search />
+        </Button>
+        <Button className="md:hidden" size={"icon"} variant={"ghost"}>
+          <Mic />
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+          <Upload />
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+          <BellIcon />
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+          <User />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PageHeader;
